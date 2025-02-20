424a5ca1561d32fba2dedda5dca087d74b10c387
Juan Sued <juansued19@gmail.com>
2025-02-20 06:58:15 -0300

    first commit

---
+++ aula1/helloWorld/hello-world.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello World!\n");
+    return 0;
+}
+
+
+//gcc hello-world.c -o hello-world-executable para criar o arquivo compilado
+//./hello-world-executable para executar o arquivo compilado
+//gcc hello-world.c -o hello-world-executable && ./hello-world-executable para compilar e executar o arquivo compilado
